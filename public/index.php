a34e86178bf248ab4d23657ce3a1e3fd9908b00f
Richard Hagen <richard.hagen@gmail.com>
2022-10-09 14:58:06 +0200

    added road-runner config

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ use Symfony\Component\Dotenv\Dotenv;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 $dotenv = new Dotenv();
-$dotenv->loadEnv(__DIR__.'/../.env');
+$dotenv->usePutenv(true)->loadEnv(__DIR__.'/../.env');
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
